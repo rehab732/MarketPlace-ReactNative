97d43e4e3c3e590b6a2ddcabc215ce2b12a978dc
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-06-23 11:21:52 +0300

    adding fonts and images and static data

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
 
 export default function App() {
+
+const[fontsLoaded]=useFonts({
+  'Inter-Bold':require('./assets/fonts/Inter-Bold.ttf'),
+  'Inter-Italic':require('./assets/fonts/Inter-Italic.ttf'),
+  'Inter-Light':require('./assets/fonts/Inter-Light.ttf'),
+  'Inter-Medium':require('./assets/fonts/Inter-Medium.ttf'),
+  'Inter-Reqular':require('./assets/fonts/Inter-Regular.ttf'),
+  'Inter-SemiBold':require('./assets/fonts/Inter-SemiBold.ttf'),
+
+
+});
+if(!fontsLoaded) return null;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar style="light" animated={true} />
+      <Text style={{fontFamily:'Inter-Italic' , fontSize:40}}>Text Test</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
